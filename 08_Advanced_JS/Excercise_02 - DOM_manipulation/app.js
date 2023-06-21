dc4a4a94867cb5a7118c4de93108574d8803de5c
marmaraja <marvlaskovic@gmail.com>
2023-06-21 22:41:03 +0200

    move

--- 08_Advanced_JS/Excercise_02 - DOM_manipulation/app.js
+++ 08_Advanced_JS/Excercise_02 - DOM_manipulation/app.js
@@ -15,7 +15,7 @@ anchorElement.href = 'https://academind.com';
 
 let newAnchorElement = document.createElement('a');
 newAnchorElement.href = 'https://google.com';
-newAnchorElement.textContent = ' This leads to Google!';
+newAnchorElement.textContent = 'This leads to Google!';
 
 // 2. Get access to the parent element that should hold the new element
 
@@ -24,3 +24,17 @@ let firstParagraph = document.querySelector('p');
 // 3. Insert the new element into the parent element content
 
 firstParagraph.append(newAnchorElement);
+
+// REMOVE ELEMENTS
+// 1. Select the element that should be removed
+
+let firstH1Element = document.querySelector('h1');
+
+// 2. Remove it!
+
+// firstH1Element.remove();
+firstH1Element.parentElement.removeChild(firstH1Element); // for older browsers
+
+// MOVE ELEMENTS
+
+firstParagraph.parentElement.append(firstParagraph);
